86f88a8b9c4f314f345c90b1c3f8542014f99e7d
Gshock3700 <161574081+Gshock3700@users.noreply.github.com>
2024-08-04 09:58:00 -0500

    Update BeeSwarm.lua

--- BeeSwarm.lua
+++ BeeSwarm.lua
@@ -1,6 +1,5 @@
 -- Load additional scripts
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Historia00012/HISTORIAHUB/main/BSS%20FREE"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Gshock3700/BSS/main/BeeSwarm.lua"))()
 
 -- Configuration
 local CONFIG = {
